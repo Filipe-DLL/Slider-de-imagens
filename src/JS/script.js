338a5945fa359a97c511230870af197a342a6372
Filipe-DLL <filipeferreira.new@gmail.com>
2022-11-29 22:43:38 -0300

    Slider-de-imagens

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,25 +1,32 @@
 let lista = document.querySelectorAll(".item")
 console.log(lista);
 
-
 function btnEsquerda() {
-   let exibido = document.querySelector(".exibir")
+    let exibido = document.querySelector(".exibir")
+    let x = 0
+    
+    lista.forEach(indice => {
 
-    for (let i = 0; i < lista.length; i++) {
-        if (exibido == lista[i]) {
+        if (exibido == indice) {
             exibido.classList.remove("exibir")
-            lista[i-1].classList.add("exibir")
+            lista[x - 1].classList.add("exibir")
         }
-    }
+        x++
+    })
 }
+
+
 
 function btnDireita() {
     let exibido = document.querySelector(".exibir")
+    let x = 0
 
-    for (let i = 0; i < lista.length; i++) {
-        if (exibido == lista[i]) {
+    lista.forEach(indice => {
+
+        if (exibido == indice) {
             exibido.classList.remove("exibir")
-            lista[i+1].classList.add("exibir")
+            lista[x + 1].classList.add("exibir")
         }
-    }
+        x++
+    })
 }
